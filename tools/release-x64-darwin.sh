f7ea5a6e594dc2fa38e2a9b16d814fbfa0beaf54
Jan Nyman <jan.nyman@controlthings.fi>
2019-02-12 18:02:01 +0200

    osx build script

---
+++ tools/release-x64-darwin.sh
@@ -0,0 +1,13 @@
+cd ..;
+VERSION=`git describe --tags --dirty`;
+if [ -z "$VERSION" ]; then
+VERSION=`cat VERSION`
+fi
+mkdir build;
+cd build;
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_IA32=OFF ..;
+make;
+TARGET=wish-core-${VERSION}-x64-darwin
+cp wish-core $TARGET
+strip $TARGET;
+cd ../tools
