4515f3ce8150005d99b99d4c71fbce9999356a83
Jan Nyman <jan.nyman@controlthings.fi>
2019-02-12 09:51:59 +0200

    Allow 512 identities, and 512 connections, maximal RPC buffer

--- port/unix/wish_port_config.h
+++ port/unix/wish_port_config.h
@@ -21,13 +21,13 @@
 
 /** This specifies the maximum number of simultaneous Wish connections
  * */
-#define WISH_PORT_CONTEXT_POOL_SZ   100
+#define WISH_PORT_CONTEXT_POOL_SZ   512
 
 /** This specifies the maximum number of simultaneous app requests to core */
 #define WISH_PORT_APP_RPC_POOL_SZ ( 60 )
 
 /** This specifies the maximum size of the buffer where some RPC handlers build the reply (1400) */
-#define WISH_PORT_RPC_BUFFER_SZ ( 16*1024 )
+#define WISH_PORT_RPC_BUFFER_SZ ( 65*1024 )
 
 /** This defines the maximum number of entries in the Wish local discovery table (4).
  * You should make sure that in the worst case any message will fit into WISH_PORT_RPC_BUFFFER_SZ  */
@@ -35,7 +35,7 @@
 
 /** This defines the maximum number of uids in database (max number of identities + contacts) (4) 
      You should make sure that in the worst case any message will fit into WISH_PORT_RPC_BUFFFER_SZ */
-#define WISH_PORT_MAX_UIDS ( 128 ) /* identity.list: 128 uid entries should fit into 16k RPC buffer */
+#define WISH_PORT_MAX_UIDS ( 512 ) /* identity.list: 128 uid entries should fit into 16k RPC buffer */
 
 
 /** If this is defined, include support for the App TCP server */
