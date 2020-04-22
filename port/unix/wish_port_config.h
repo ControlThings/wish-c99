a3fb63676c3a5374a1d87a2a45256dfcd475b100
Jan Nyman <jan.nyman@controlthings.fi>
2020-04-22 13:39:33 +0300

    Allow larger amount of connections and uids

--- port/unix/wish_port_config.h
+++ port/unix/wish_port_config.h
@@ -19,24 +19,24 @@
 /** This specifies the size of the receive ring buffer */
 #define WISH_PORT_RX_RB_SZ ( 32*1024 )
 
-/** This specifies the maximum number of simultaneous Wish connections
+/** This specifies the maximum number of simultaneous Wish connections.
+ * 
+ * Unix port uses select(2), which has limitation FD_SETSIZE = 1024, and all filedescriptors must also have value less than this.
+ * So number of wish connections, and app connections, and open files at any moment must be less than 1024.
  * */
-#define WISH_PORT_CONTEXT_POOL_SZ   512
+#define WISH_PORT_CONTEXT_POOL_SZ   768
 
 /** This specifies the maximum number of simultaneous app requests to core */
 #define WISH_PORT_APP_RPC_POOL_SZ ( 60 )
 
-/** This specifies the maximum size of the buffer where some RPC handlers build the reply (1400) */
+/** This specifies the maximum size of the buffer where some RPC handlers build the reply */
 #define WISH_PORT_RPC_BUFFER_SZ ( 65*1024 )
 
-/** This defines the maximum number of entries in the Wish local discovery table (4).
- * You should make sure that in the worst case any message will fit into WISH_PORT_RPC_BUFFFER_SZ  */
-#define WISH_LOCAL_DISCOVERY_MAX ( 64 ) /* wld.list: 64 local discoveries should fit in 16k RPC buffer size */
+/** This defines the maximum number of entries in the Wish local discovery table */
+#define WISH_LOCAL_DISCOVERY_MAX ( 64 )
 
-/** This defines the maximum number of uids in database (max number of identities + contacts) (4) 
-     You should make sure that in the worst case any message will fit into WISH_PORT_RPC_BUFFFER_SZ */
-#define WISH_PORT_MAX_UIDS ( 512 ) /* identity.list: 128 uid entries should fit into 16k RPC buffer */
-
+/** This defines the maximum number of uids in database (max number of identities + contacts) */
+#define WISH_PORT_MAX_UIDS ( 2048 )
 
 /** If this is defined, include support for the App TCP server */
 #define WITH_APP_TCP_SERVER
